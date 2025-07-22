ba31a9d5b43dffc790419332601936bd52829835
Edriene Paingan <painganedrienejames@gmail.com>
2025-07-22 16:29:37 +0800

    Flow Layout Practice

---
+++ FlowLayout/src/Main.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+        frame.setLayout(new FlowLayout(FlowLayout.TRAILING, 10, 10));
+
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(100, 250));
+        panel.setBackground(Color.lightGray);
+        panel.setLayout(new FlowLayout());
+
+        panel.add(new JButton("1"));
+        panel.add(new JButton("2"));
+        panel.add(new JButton("3"));
+        panel.add(new JButton("4"));
+        panel.add(new JButton("5"));
+        panel.add(new JButton("6"));
+        panel.add(new JButton("7"));
+        panel.add(new JButton("8"));
+        panel.add(new JButton("9"));
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
